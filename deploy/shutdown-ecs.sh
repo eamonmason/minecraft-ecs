f0611b3be1e1e342e205517509431035e441545a
Eamon Mason <eamon.mason@thomsonreuters.com>
2024-11-13 22:28:00 +0000

    fix: explicitly resize ecs service to zero before destroying CDK stack to ensure SIGTERM sent to container

--- deploy/shutdown-ecs.sh
+++ deploy/shutdown-ecs.sh
@@ -2,7 +2,17 @@
 
 
 # Set AWS region
-AWS_REGION="eu-west-2"
+source .env
+
+# Set ECS cluster and service names based on naming convention
+ECS_CLUSTER_ARN=$(aws ecs list-clusters --region "$AWS_REGION" --query "clusterArns" --output text | grep MinecraftCluster)
+ECS_SERVICE_ARN=$(aws ecs list-services --cluster "$ECS_CLUSTER_ARN" --region "$AWS_REGION" --query "serviceArns" --output text | grep MinecraftService)
+
+# Update the ECS service to set desired count to zero
+aws ecs update-service --cluster "$ECS_CLUSTER_ARN" --service "$ECS_SERVICE_ARN" --desired-count 0 --region "$AWS_REGION"
+
+# Wait for the service to scale down to zero tasks
+aws ecs wait services-stable --cluster "$ECS_CLUSTER_ARN" --services "$ECS_SERVICE_ARN" --region "$AWS_REGION"
 
 # Destroy the CDK stack without confirmation
 cdk destroy --force --region "$AWS_REGION"
--- deploy/startup-ecs.sh
+++ deploy/startup-ecs.sh
@@ -8,8 +8,8 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # Set variables
+source .env
 MINECRAFT_WORLD=$1
-AWS_REGION="eu-west-2"
 SSM_PARAMETER_NAME="/minecraft/mcServerName"
 
 # Update the SSM parameter
